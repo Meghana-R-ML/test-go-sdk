d2376a8c52bd0ad96d7f2e3dd405b736c91b97b1
Nidhi1700 <92188564+Nidhi1700@users.noreply.github.com>
2022-09-08 21:35:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-
-	intersight "github.com/CiscoDevNet/intersight-go"
+// 	"context"
+// 	"fmt"
+// 	"log"
+	example "main/Examples"
+// 	intersight "github.com/CiscoDevNet/intersight-go"
 )
 
 func main() {
@@ -13,41 +13,51 @@ func main() {
 	keyFile := "API_KEY_PATH"
 	host := "TARGET_SERVER"
 
-
 	// Set up the authentication configuration struct
-	authCfg := intersight.HttpSignatureAuth{
-		KeyId:          keyID,
-		PrivateKeyPath: keyFile,
+	// authCfg := intersight.HttpSignatureAuth{
+	// 	KeyId:          keyID,
+	// 	PrivateKeyPath: keyFile,
 
-		SigningScheme: intersight.HttpSigningSchemeRsaSha256,
-		SignedHeaders: []string{
-			intersight.HttpSignatureParameterRequestTarget, 
-			"Host",
-			"Date",
-			"Digest",
-		},
-		SigningAlgorithm: intersight.HttpSigningAlgorithmRsaPKCS1v15,
-	}
+	// 	SigningScheme: intersight.HttpSigningSchemeRsaSha256,
+	// 	SignedHeaders: []string{
+	// 		intersight.HttpSignatureParameterRequestTarget,
+	// 		"Host",
+	// 		"Date",
+	// 		"Digest",
+	// 	},
+	// 	SigningAlgorithm: intersight.HttpSigningAlgorithmRsaPKCS1v15,
+	// }
 
+	// ctx, err := authCfg.ContextWithValue(context.Background())
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	log.Fatal("Error creating authentication context")
+	// }
 
-	ctx, err := authCfg.ContextWithValue(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		log.Fatal("Error creating authentication context")
-	}
+	//     config := intersight.NewConfiguration()
+	//     config.Host = host
+	//     config.Debug = true
 
-        config := intersight.NewConfiguration()
-        config.Host = host
-        config.Debug = true
+	//     client := intersight.NewAPIClient(config)
 
-        client := intersight.NewAPIClient(config)
+	// res, _, err := client.SnmpApi.GetSnmpPolicyList(ctx).Execute()
+	// if err != nil {
+	// 	log.Fatalf("Error getting SNMP policies: %v", err)
+	// }
 
-	res, _, err := client.SnmpApi.GetSnmpPolicyList(ctx).Execute()
-	if err != nil {
-		log.Fatalf("Error getting SNMP policies: %v", err)
-	}
+	// for _, snmpPolicy := range res.SnmpPolicyList.Results {
+	// 	fmt.Printf("Snmp Policy: %s\n", *snmpPolicy.Name)
+	// }
 
-	for _, snmpPolicy := range res.SnmpPolicyList.Results {
-		fmt.Printf("Snmp Policy: %s\n", *snmpPolicy.Name)
-	}
+	//invoking the CreateObject to create a new instance of the bootPrecisionPolicy
+	example.CreateObject(keyID, keyFile, host)
+
+	//invoke getObjectList to get list of smtp policy
+	example.GetObjectList(keyID, keyFile, host)
+
+	//invoke getOjectListWithFilter
+	example.GetObjectListWithFilter(keyID, keyFile, host)
+
+	//invoke GetObjectWithMoid to get list of smtp policy by moid
+	example.GetObjectWithMoid(keyID, keyFile, host)
 }
