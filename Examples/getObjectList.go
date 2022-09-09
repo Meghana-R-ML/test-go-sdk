0e545375e6912e8da23bd30d6a7e6308eda5ca3d
Nidhi1700 <92188564+Nidhi1700@users.noreply.github.com>
2022-09-09 15:22:10 +0530

    Update getObjectList.go

--- Examples/getObjectList.go
+++ Examples/getObjectList.go
@@ -3,7 +3,7 @@ package example
 import (
 	"fmt"
 	"os"
-
+	"log"
 	//intersight "github.com/CiscoDevNet/intersight-go"
 	"context"
 )
@@ -17,13 +17,18 @@ import (
 func GetObjectList(apiKey string, SecretKey string, endpoint string) {
 	apiClient, err := getApiClient(apiKey, SecretKey, endpoint)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+		log.Printf("Error while getting api client: %v\n", err)
+		return
 	}
 
 	apiResponse, r, err := apiClient.SmtpApi.GetSmtpPolicyList(context.Background()).Execute()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `SmtpApi.GetSmtpPolicyList``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+// 		fmt.Fprintf(os.Stderr, "Error when calling `SmtpApi.GetSmtpPolicyList``: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		log.Printf("Error when calling `SmtpApi.GetSmtpPolicyList``: %v\n", err)
+		log.Printf("Full HTTP response: %v\n", r)
+		return
 	}
 	fmt.Fprintf(os.Stdout, "Response from `SmtpApi.GetSmtpPolicyList`: %v\n", apiResponse)
 }
