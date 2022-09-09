3d8841d8f592430fcc586f7c416ade27d11b21af
Nidhi1700 <92188564+Nidhi1700@users.noreply.github.com>
2022-09-09 15:26:45 +0530

    Update getObjectWithMoid.go

--- Examples/getObjectWithMoid.go
+++ Examples/getObjectWithMoid.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"log"
 	//intersight "github.com/CiscoDevNet/intersight-go"
 )
 
@@ -18,12 +19,17 @@ func GetObjectWithMoid(apiKey string, SecretKey string, endpoint string) {
 
 	apiClient, err := getApiClient(apiKey, SecretKey, endpoint)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+		log.Printf("Error while getting api client: %v\n", err)
+		return
 	}
 	apiResponse, r, err := apiClient.SmtpApi.GetSmtpPolicyByMoid(context.Background(), moid).Execute()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `SmtpApi.GetSmtpPolicyByMoid``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+// 		fmt.Fprintf(os.Stderr, "Error when calling `SmtpApi.GetSmtpPolicyByMoid``: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		log.Printf("Error when calling `SmtpApi.GetSmtpPolicyByMoid``: %v\n", err)
+		log.Printf("Full HTTP response: %v\n", r)
+		return
 	}
 
 	fmt.Fprintf(os.Stdout, "Response from `SmtpApi.GetSmtpPolicyByMoid`: %v\n", apiResponse)
