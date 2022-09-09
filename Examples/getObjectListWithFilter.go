a6bcea8bf22b0fc904e05a0471f4d33d0e12f189
Nidhi1700 <92188564+Nidhi1700@users.noreply.github.com>
2022-09-09 15:24:30 +0530

    Update getObjectListWithFilter.go

--- Examples/getObjectListWithFilter.go
+++ Examples/getObjectListWithFilter.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"log"
 	//intersight "github.com/CiscoDevNet/intersight-go"
 )
 
@@ -28,12 +29,17 @@ func GetObjectListWithFilter(apiKey string, SecretKey string, endpoint string) {
 
 	apiClient, err := getApiClient(apiKey, SecretKey, endpoint)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
+		log.Printf("Error while getting api client: %v\n", err)
+		return
 	}
 	apiResponse, r, err := apiClient.ComputeApi.GetComputeRackUnitList(context.Background()).Filter(filter).Orderby(orderby).Top(top).Skip(skip).Select_(select_).Expand(expand).Apply(apply).Count(count).Inlinecount(inlinecount).At(at).Tags(tags).Execute()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
-		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+// 		fmt.Fprintf(os.Stderr, "Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
+// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		log.Printf("Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
+		log.Printf("Full HTTP response: %v\n", r)
+		return
 	}
 
 	fmt.Fprintf(os.Stdout, "Response from `ComputeApi.GetComputeRckUnitList`: %v\n", apiResponse)
