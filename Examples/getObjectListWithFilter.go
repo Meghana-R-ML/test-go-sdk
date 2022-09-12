ac27834d075daaaf7999ccae92d6b99ee68b186c
Nidhi1700 <92188564+Nidhi1700@users.noreply.github.com>
2022-09-12 09:37:24 +0530

    Update getObjectListWithFilter.go

--- Examples/getObjectListWithFilter.go
+++ Examples/getObjectListWithFilter.go
@@ -1,42 +1,32 @@
 package example
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"log"
-	//intersight "github.com/CiscoDevNet/intersight-go"
+	"os"
+	intersight "github.com/CiscoDevNet/intersight-go"
 )
 
-// const (
-// 	apiKey    = ""
-// 	SecretKey = ""
-// 	endpoint  = "https://intersight.com"
-// )
-
-func GetObjectListWithFilter(apiKey string, SecretKey string, endpoint string) {
+func GetObjectListWithFilter(config *Config) {
 	filter := "CreateTime gt 2021-08-29T21:58:33Z"
 	orderby := "CreateTime"
 	top := int32(5)
 	skip := int32(1)
 	select_ := "CreateTime,ModTime"
-// 	expand := "Biosunits"
+	// 	expand := "Biosunits"
 	apply := "groupby((Model),aggregate(AvailableMemory with min as MinAvailableMemory))"
 	count := false
 	inlinecount := "allpages"
-// 	at := "VersionType eq 'Configured'"
-// 	tags := "tags_example"
+	// 	at := "VersionType eq 'Configured'"
+	// 	tags := "tags_example"
 
-	apiClient, err := getApiClient(apiKey, SecretKey, endpoint)
+	cfg := getApiClient(config)
+	apiClient := cfg.ApiClient
+	ctx := cfg.ctx
+	apiResponse, r, err := apiClient.ComputeApi.GetComputeRackUnitList(ctx).Filter(filter).Orderby(orderby).Top(top).Skip(skip).Select_(select_).Apply(apply).Count(count).Inlinecount(inlinecount).Execute()
 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Error while getting api client: %v\n", err)
-		log.Printf("Error while getting api client: %v\n", err)
-		return
-	}
-	apiResponse, r, err := apiClient.ComputeApi.GetComputeRackUnitList(context.Background()).Filter(filter).Orderby(orderby).Top(top).Skip(skip).Select_(select_).Apply(apply).Count(count).Inlinecount(inlinecount).Execute()
-	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
-// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		// 		fmt.Fprintf(os.Stderr, "Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
+		// 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
 		log.Printf("Error when calling `ComputeApi.GetComputeRckUnitList``: %v\n", err)
 		log.Printf("Full HTTP response: %v\n", r)
 		return
